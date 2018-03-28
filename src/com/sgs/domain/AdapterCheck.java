2d5edc4558e2360506e3af193de07e47c2327587
prabhat <govindgaurav1991@gmail.com>
2018-03-29 00:24:20 +0530

    Here we are adding emp Name,age,Id

--- src/com/sgs/domain/AdapterCheck.java
+++ src/com/sgs/domain/AdapterCheck.java
@@ -5,19 +5,19 @@ public class AdapterCheck implements ShowInfo {
 	@Override
 	public void getName() {
 		// TODO Auto-generated method stub
-		
+		 System.out.println(" emp name is: prabhat");
 	}
 
 	@Override
 	public void getAge() {
 		// TODO Auto-generated method stub
-		
+		System.out.println(" emp age is: 26");
 	}
 
 	@Override
 	public void getId() {
 		// TODO Auto-generated method stub
-		
+		System.out.println(" emp id is: Pio1245");
 	}
 
 	@Override
